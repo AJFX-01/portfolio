f9c34637c08cb2c79bc14e133a92a120ead3bfab
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-05-26 09:15:14 -0700

    moved files arround

---
+++ services.js
@@ -0,0 +1,17 @@
+// Arrays Of Services 
+
+const services = [
+    {image: 'images/coding (1).png', name: 'Web Development'},
+
+    {image: 'images/icon2.jpeg', name:'Web Design'},
+
+    {image: 'images/iconsD.jpeg', name:'Mechanical Enginner'},
+
+    {image: 'images/analytics-laptop (1).png', name:'Data Enginner'},
+
+    {image: 'images/iconsD2.jpeg', name:'Data Science'},
+
+    {image: 'images/icons3.jpeg', name:'Mobile App Development'},
+
+];
+
